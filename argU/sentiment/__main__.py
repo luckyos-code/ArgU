70b8264be85163aa69e5eb44109034dda75223bb
Lucas <luckyos-code@users.noreply.github.com>
2020-01-03 17:27:58 +0100

    Add csv with analysed topic sentiments

--- argU/sentiment/__main__.py
+++ argU/sentiment/__main__.py
@@ -2,11 +2,12 @@ from sentiment.analysis import get_nltk_data, run as nltk_run
 from sentiment.google import run as google_run
 from utils.reader import read_csv, read_csv_header
 
-import os, rootpath
+import os, rootpath, csv
 
 ROOT_PATH = rootpath.detect()
 RESOURCES_PATH = os.path.join(ROOT_PATH, 'resources/')
 TOPICS_PATH = os.path.join(RESOURCES_PATH, 'topics.csv')
+TOPICS_AUTOMATIC_PATH = os.path.join(RESOURCES_PATH, 'topics-automatic.csv')
 TITLES_PATH = os.path.join(RESOURCES_PATH, 'args-me.csv')
 
 def nltk_topics():
@@ -18,7 +19,12 @@ def nltk_titles():
     nltk_run(read_csv(TITLES_PATH, 100), 'titles')
 
 def google_topics():
-    google_run(read_csv(TOPICS_PATH, max_rows=-1), os.path.join(ROOT_PATH, 'argU/sentiment/topic_log'), 'topics')
+    topics = []
+    for row in read_csv(TOPICS_AUTOMATIC_PATH, max_rows=-1):
+        topics.append(row[1])
+    for row in read_csv(TOPICS_PATH, max_rows=-1):
+        topics.append(row[5] + '.')
+    google_run(topics, os.path.join(ROOT_PATH, 'argU/sentiment/topic_sentiments.csv'), os.path.join(ROOT_PATH, 'argU/sentiment/topic_log'), 'topics')
     
 def google_titles():
     google_run(read_csv(TITLES_PATH, 100), os.path.join(ROOT_PATH, 'argU/sentiment/title_log'), 'titles')
@@ -28,7 +34,10 @@ if __name__ == "__main__":
     #topicEntry = next(read_csv(TOPICS_PATH, 1))
     #print(topicEntry[5])
     #print(read_csv_header(TITLES_PATH)) # [2] is source_id, [8] is id, [5] is discussion_title
-    #titleEntry = next(read_csv(TITLES_PATH, 1))
-    #print(titleEntry[5])
+    #titles = []
+    #for titleEntry in read_csv(TITLES_PATH, -1):
+    #    titles.append(titleEntry[5])
+    #titles = list(dict.fromkeys(titles))
+    #print(len(titles))
     #google_topics()
-    nltk_topics()
+    #nltk_titles()
--- argU/sentiment/google.py
+++ argU/sentiment/google.py
@@ -1,13 +1,9 @@
 from google.cloud import language_v1
 from google.cloud.language_v1 import enums
-import logging
+import logging, csv
 
 def getQueryText(rows):
-    text = ''
-    for row in rows:
-        query_text = row [5] + '. '
-        text += query_text
-    return text[:-1]
+    return ' '.join(rows)
 
 def getTitleText(rows):
     text = ''
@@ -16,7 +12,7 @@ def getTitleText(rows):
         text += query_text
     return text[:-1]
 
-def run(rows, logpath, mode):
+def run(rows, csvpath, logpath, mode):
     """
     Analyzing Sentiment in a String
 
@@ -46,27 +42,43 @@ def run(rows, logpath, mode):
 
     response = client.analyze_sentiment(document, encoding_type=encoding_type)
     # Get overall sentiment of the input document
-    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
-    print(
-        u"Document sentiment magnitude: {}\n".format(
-            response.document_sentiment.magnitude
-        )
-    )
+    #print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    #print(
+    #    u"Document sentiment magnitude: {}\n".format(
+    #        response.document_sentiment.magnitude
+    #    )
+    #)
     # Get sentiment for all sentences in the document
-    for sentence in response.sentences:
+    for num, sentence in enumerate(response.sentences, start=1):
+        if sentence.text.content[-1:] == '.':
+            sentence.text.content = sentence.text.content[:-1]
+        print(u"Sentence number: {}".format(num))
         print(u"Sentence text: {}".format(sentence.text.content))
         print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
         print(u"Sentence sentiment magnitude: {}\n".format(sentence.sentiment.magnitude))
-        
+    
+    if mode == 'topics':
+        text_content = getQueryText(rows)
+        with open(csvpath, mode='w') as sentiments_csv:
+            sentiment_writer = csv.writer(sentiments_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            sentiment_writer.writerow(['num', 'text', 'sentiment_score', 'sentiment_magnitude'])
+            for num, sentence in enumerate(response.sentences, start=1):
+                if sentence.text.content[-1:] == '.':
+                    sentence.text.content = sentence.text.content[:-1]
+                sentiment_writer.writerow([num, sentence.text.content, sentence.sentiment.score, sentence.sentiment.magnitude])
+    
     logging.basicConfig(level=logging.DEBUG, filename=logpath, filemode="a+",
                         format="%(message)s")
-    logging.info(u"Document sentiment score: {}".format(response.document_sentiment.score))
-    logging.info(
-        u"Document sentiment magnitude: {}\n".format(
-            response.document_sentiment.magnitude
-        )
-    )
-    for sentence in response.sentences:
+    #logging.info(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    #logging.info(
+    #    u"Document sentiment magnitude: {}\n".format(
+    #        response.document_sentiment.magnitude
+    #    )
+    #)
+    for num, sentence in enumerate(response.sentences, start=1):
+        if sentence.text.content[-1:] == '.':
+            sentence.text.content = sentence.text.content[:-1]
+        logging.info(u"Sentence number: {}".format(num))
         logging.info(u"Sentence text: {}".format(sentence.text.content))
         logging.info(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
         logging.info(u"Sentence sentiment magnitude: {}\n".format(sentence.sentiment.magnitude))
