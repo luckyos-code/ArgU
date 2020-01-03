aa0fbc0b60e1af95b7872cc124cb603671d7baf1
Christian Staudte <cs47rica@studserv.uni-leipzig.de>
2020-01-03 16:44:34 +0100

    Test refactoring

--- tests/bm25_test.py
+++ tests/bm25_test.py
@@ -13,26 +13,23 @@ from utils.beautiful import print_argument_texts
 ROOT_PATH = rootpath.detect()
 RESOURCES_PATH = os.path.join(ROOT_PATH, 'resources/')
 CSV_PATH = os.path.join(RESOURCES_PATH, 'args-me.csv')
+BM25_PATH = os.path.join(RESOURCES_PATH, 'bm25.model')
 
 first_n_args = 5000
 query = "Trump"
 
-bm25 = BM25Manager()
-bm25.build(CSV_PATH, max_args=first_n_args)
+train = False
+if train:
+    bm25 = BM25Manager()
+    bm25.build(CSV_PATH, max_args=first_n_args)
+    bm25.store(BM25_PATH)
+else:
+    bm25 = BM25Manager.load(BM25_PATH)
 
 scores_dict = bm25.norm_scores(query)
-# print(scores_dict)
 best_arg_id = max(scores_dict.items(), key=operator.itemgetter(1))[0]
 
 # best_score = np.max(np.array(list(scores_dict.values())))
 # print(best_score)
-# sys.exit(0)
 
-# best_arg = ''
-# for (arg_id, score) in scores.items():
-# if score == best_score:
-# best_arg = arg_id
-# break
-# print(best_arg)
-
-# print_argument_texts([best_arg_id], CSV_PATH, print_all=True)
+print_argument_texts([best_arg_id], CSV_PATH, print_all=True)
