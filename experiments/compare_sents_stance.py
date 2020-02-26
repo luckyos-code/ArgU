f5de9170a9f492a989884cd810a171f082fe2111
luckyos-code <luckyos-code@users.noreply.github.com>
2020-02-26 15:37:06 +0100

    Further experimenting

--- experiments/compare_sents_stance.py
+++ experiments/compare_sents_stance.py
@@ -17,31 +17,35 @@ coll_train = db[setup.MONGO_DB_COL_TRAIN]
 coll_sents = db[setup.MONGO_DB_COL_SENTIMENTS]
 
 values = {}
-max_args = 1
+max_args = -1
+
+counAll = countProN = countConN = countProNeg = countConPos = countConNeg = countProPos = 0
 
 for i, ct in tqdm(enumerate(coll_train.find())):
     if i == max_args:
         break
     sent = coll_sents.find_one({'_id': ct['_id']})
     arg = coll_args.find_one({'_id': ct['_id']})
-    print(arg)
-    values[ct['_id']] = (arg['premises'][0]['stance'], sent['score'])
-print(values)
+    if sent['score'] < -0.1:
+        if arg['premises'][0]['stance'] == 'PRO':
+            countProNeg += 1
+        else:
+        	countConNeg += 1
+    elif sent['score'] > 0.1:
+        if arg['premises'][0]['stance'] == 'CON':
+            countConPos += 1
+        else:
+        	countProPos += 1
+    else:
+        if arg['premises'][0]['stance'] == 'PRO':
+            countProN += 1
+        else:
+            countConN += 1
+    counAll += 1
 
-# countProN = countConN = countProNeg = countConPos = 0
-# for arg in tqdm(coll_sents.find()):
-#     if arg['score'] < -0.1:
-#         if values[arg['_id']]['stance'] == 'PRO':
-#             countProNeg += 1
-#     elif arg['score'] > 0.1:
-#         if values[arg['_id']]['stance'] == 'CON':
-#             countConPos += 1
-#     else:
-#         if values[arg['_id']]['stance'] == 'PRO':
-#             countProN += 1
-#         else:
-#             countConN += 1
-
-# print(f'Original stance for neutral sents: {countProN} Pro, {countConN} Con')
-# print(f'Pro args for negative sents: {countProNeg}')
-# print(f'Con args for positive sents: {countConPos}')
+print(f'NEW Original stance for neutral sents: {countProN} Pro, {countConN} Con')
+print(f'CORRECT Con args for negative sents: {countConNeg}')
+print(f'FALSE Pro args for negative sents: {countProNeg}')
+print(f'CORRECT Pro args for positive sents: {countProPos}')
+print(f'FALSE Con args for positive sents: {countConPos}')
+print(f'Gesamt: {counAll}')
