9fdec8fec297f7c97da5f785b3e109289721a836
Christian Staudte <christianstaudte@freenet.de>
2020-08-15 16:59:13 +0200

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@ import os
 import rootpath
 
 ROOT_PATH = rootpath.detect()
+OUTPUT_PATH = os.path.join(ROOT_PATH)
+
 RESOURCES_PATH = os.path.join(ROOT_PATH, 'resources/')
-
-if not os.path.isdir(RESOURCES_PATH):
-    os.mkdir(RESOURCES_PATH)
-
 IMAGES_PATH = os.path.join(RESOURCES_PATH, 'images/')
-STOPWORDS_PATH = os.path.join(ROOT_PATH, 'stopwords_eng.txt')
+STOPWORDS_PATH = os.path.join(RESOURCES_PATH, 'stopwords_eng.txt')
+TREC_PATH = os.path.join(RESOURCES_PATH, 'args-me.trec')
+TREC_PATH_TOPICS = os.path.join(RESOURCES_PATH, 'topics.trec')
+CBOW_PATH = os.path.join(RESOURCES_PATH, 'cbow.model')
+TERRIER_RESULTS_PATH = os.path.join(RESOURCES_PATH, 'terrier.res')
+
+SENTIMENTS_PATH = os.path.join(ROOT_PATH, 'argU', 'sentiment', 'results', 'argument_sentiments.csv')
+SENTIMENTS_CONFIG = {
+    'delimiter': ',',
+    'quotechar': '"',
+    'quoting': csv.QUOTE_MINIMAL,
+}
 
 MONGO_DB_NAME = 'argU'
 MONGO_DB_URL = 'mongodb://localhost:27017/'
@@ -22,21 +31,6 @@ MONGO_DB_COL_EMBEDDINGS = 'emb'
 MONGO_DB_COL_EMBEDDINGS_BACKUP = 'emb_back'
 MONGO_DB_COL_RESULTS = 'results'
 MONGO_DB_COL_RESULTS_BACKUP = 'results_back'
-
-TREC_PATH = os.path.join(RESOURCES_PATH, 'args-me.trec')
-TREC_PATH_TOPICS = os.path.join(RESOURCES_PATH, 'topics.trec')
-CBOW_PATH = os.path.join(RESOURCES_PATH, 'cbow.model')
-TERRIER_RESULTS_PATH = os.path.join(RESOURCES_PATH, 'terrier.res')
-OUTPUT_PATH = os.path.join(ROOT_PATH)
-
-SENTIMENTS_PATH = os.path.join(
-    ROOT_PATH, 'argU/sentiment/results/argument_sentiments.csv'
-)
-SENTIMENTS_CONFIG = {
-    'delimiter': ',',
-    'quotechar': '"',
-    'quoting': csv.QUOTE_MINIMAL,
-}
 
 METHOD_NO = 'ulT1DetroitnitzCbowDPHSentNo'
 METHOD_EMOTIONAL = 'ulT1DetroitnitzCbowDPHSentEmotional'
