39dfeeb635cff5e0c83e8b5c216addf3375a0287
Lucas Lange <luckyos-code@users.noreply.github.com>
2020-08-24 19:34:09 +0200

    add parameter for top n arguments

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-while getopts e:s:o: option
+while getopts n:e:s:o: option
 do
 case "${option}"
 in
+n) topN=${OPTARG};;
 e) embedding=${OPTARG};;
 s) runType=${OPTARG};;
 o) outputDir=${OPTARG};;
 esac
 done
 
-docker run -e EMBEDDING=$embedding -e RUN_TYPE=$runType -v $outputDir:/output --name argu --rm -it argu
+docker run -e TOP_N=$topN -e EMBEDDING=$embedding -e RUN_TYPE=$runType -v $outputDir:/output --name argu --rm -it argu
